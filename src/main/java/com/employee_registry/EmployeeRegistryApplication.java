65fb0c7127bde821c86b498e096c0b93b7a48e36
sangharsh001 <sangahrshkulal@gmail.com>
2025-05-18 12:42:53 +0530

    Initial commit of employee-registry

---
+++ src/main/java/com/employee_registry/EmployeeRegistryApplication.java
@@ -0,0 +1,16 @@
+package com.employee_registry;
+
+import org.springframework.boot.SpringApplication;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class EmployeeRegistryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeRegistryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/employee_registry/EmployeeRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.employee_registry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
